e7957fe105c8b18e967dffc9d9227e3a264fa7ad
Phumphathai Chansriwong <ouoam2555@gmail.com>
2020-04-22 22:52:58 +0700

    Self manage dependency

---
+++ dep.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	_ "github.com/gofiber/fiber"
+	_ "github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	_ "github.com/pkg/errors"
+	_ "golang.org/x/crypto/bcrypt"
+	_ "gopkg.in/guregu/null.v3"
+)
